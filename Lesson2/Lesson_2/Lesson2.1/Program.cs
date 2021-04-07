9c4e8c003917ee01ab2b807d14423f4687445ddf
Noloqe <korostelyuk@gmail.com>
2021-04-07 16:19:31 +0300

    lesson 2
    created homework for lesson 2

---
+++ Lesson2/Lesson_2/Lesson2.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите минимальную суточную температуру: ");
+            string minTempStr = Console.ReadLine();
+            string preparedMinTempStr = minTempStr.Replace('.', ',');
+            bool minTempParseResult = float.TryParse(preparedMinTempStr, out float minTemp);
+
+            if (minTempParseResult == false)
+            {
+                Console.WriteLine("Неккоректно введена суточная температура.");
+                return;
+            }
+            
+            Console.WriteLine("Введите максимальную суточную температуру: ");
+            string maxTempStr = Console.ReadLine();
+            string preparedMaxTempStr = maxTempStr.Replace('.', ',');
+            bool maxTempParseResult = float.TryParse(preparedMaxTempStr, out float maxTemp);
+
+            if (maxTempParseResult == false)
+            {
+                Console.WriteLine("Неккоректно введена суточная температура.");
+                return;
+            }
+
+            float averageTemp = (minTemp + maxTemp) / 2;
+            Console.WriteLine($"\nСреднесуточная температура: {averageTemp} градусов ");
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson2/Lesson_2/Lesson2.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lesson2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число: ");
+            string inputNumber = Console.ReadLine();
+            bool parseResult = int.TryParse(inputNumber, out int number);
+
+            if (parseResult == false)
+            {
+                Console.WriteLine("Некорректно введено число.");
+                return;
+            }
+
+            if ((number % 2) == 0)
+            {
+                Console.WriteLine($"{number} - это чётное число.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} - это нечётное число.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
