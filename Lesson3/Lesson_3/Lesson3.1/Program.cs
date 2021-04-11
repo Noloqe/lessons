cbd611a13497edb33df3cbd0b6f9d8499ff63974
Noloqe <korostelyuk@gmail.com>
2021-04-11 18:45:31 +0300

    lesson 3
    created homework for lesson 3

---
+++ Lesson3/Lesson_3/Lesson3.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lesson3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 } };
+            string emptyStr = "";
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    emptyStr += " ";
+                    Console.WriteLine($"{emptyStr}{array[i, j]}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
